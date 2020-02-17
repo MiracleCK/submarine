177b4320111117c5d003ac513dd5c3c5b9ef6e12
terry <terry@chasing-innovation.com>
2020-02-17 15:13:24 +0800

    only send battery status when battery is healthy

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -249,7 +249,8 @@ bool GCS_MAVLINK::send_battery_status() const
     const AP_BattMonitor &battery = AP::battery();
 
     for(uint8_t i = 0; i < battery.num_instances(); i++) {
-        if (battery.get_type(i) != AP_BattMonitor_Params::BattMonitor_Type::BattMonitor_TYPE_NONE) {
+        if (battery.healthy() &&
+        battery.get_type(i) != AP_BattMonitor_Params::BattMonitor_Type::BattMonitor_TYPE_NONE) {
             CHECK_PAYLOAD_SIZE(BATTERY_STATUS);
             send_battery_status(i);
         }
