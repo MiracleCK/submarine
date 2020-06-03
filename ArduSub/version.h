883703d599d47dc53173717804767ff1ee4ac4a7
terry <terry@chasing-innovation.com>
2020-06-03 13:01:58 +0800

    bump m2 version to 2.1.1

--- ArduSub/version.h
+++ ArduSub/version.h
@@ -6,12 +6,12 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "M2 v2.1.0dev"
+#define THISFIRMWARE "M2 v2.1.1dev"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 2,0,7,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 2,1,1,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 2
 #define FW_MINOR 1
-#define FW_PATCH 0
+#define FW_PATCH 1
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
