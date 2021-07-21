ec1566e42e5d33940679d3495d0492a8cd7f2668
yinlanshan <yinlanshan@chasing-innovation.com>
2021-07-21 10:16:49 +0800

    简化命令行的注册，去掉hal层的冗余声明

--- libraries/AP_HAL_ChibiOS/Shell.cpp
+++ libraries/AP_HAL_ChibiOS/Shell.cpp
@@ -4,6 +4,7 @@
 #include "AP_HAL_ChibiOS.h"
 
 #include "Shell.h"
+#include <shell.h>
 
 #include "hal.h"
 #include "chprintf.h"
@@ -12,35 +13,16 @@ using namespace ChibiOS;
 
 extern const AP_HAL::HAL & hal;
 
-static void cmd_param(BaseSequentialStream *chp, int argc, char *argv[]);
-static void cmd_version(BaseSequentialStream *chp, int argc, char *argv[]);
-static void cmd_reset(BaseSequentialStream *chp, int argc, char *argv[]);
-static void cmd_cali(BaseSequentialStream *chp, int argc, char *argv[]);
-static void cmd_led(BaseSequentialStream *chp, int argc, char *argv[]);
-static void cmd_mode(BaseSequentialStream *chp, int argc, char *argv[]);
-static void cmd_commands(const char* name, BaseSequentialStream *chp, int argc, char *argv[]);
-static AP_HAL::Shell::ShellCommand* _shell_commands = NULL;
-
-static ShellCommand chibi_shell_commands[] = {
-  {"param", cmd_param},
-  {"version", cmd_version},
-  {"reset", cmd_reset},
-  {"cali", cmd_cali},
-  {"led", cmd_led},
-  {"mode", cmd_mode},
-  {NULL, NULL} // this is the end of commands
-};
-
 ShellConfig shell_cfg = {
   (BaseSequentialStream *)&HAL_STDOUT_SERIAL,
-  chibi_shell_commands
+  NULL
 };
 
 AP_HAL::HAL::Callbacks* g_factory_cb = NULL;
 
 void Shell::register_commands(AP_HAL::Shell::ShellCommand* commands)
 {
-    _shell_commands = commands;
+    shell_cfg.hc_commands = (HalCommand *)commands;
 }
 
 void Shell::register_factory_cb(AP_HAL::HAL::Callbacks* cb)
@@ -58,45 +40,4 @@ int Shell::printf(const char *fmt, ...)
     va_end(ap);
 
     return formatted_bytes;
-}
-
-void cmd_param(BaseSequentialStream *chp, int argc, char *argv[]) {
-    cmd_commands("param", chp, argc, argv);
-}
-
-void cmd_version(BaseSequentialStream *chp, int argc, char *argv[]) {
-    cmd_commands("version", chp, argc, argv);
-}
-
-void cmd_reset(BaseSequentialStream *chp, int argc, char *argv[]) {
-    cmd_commands("reset", chp, argc, argv);
-}
-
-void cmd_cali(BaseSequentialStream *chp, int argc, char *argv[]) {
-    cmd_commands("cali", chp, argc, argv);
-}
-
-void cmd_led(BaseSequentialStream *chp, int argc, char *argv[]) {
-    cmd_commands("led", chp, argc, argv);
-}
-
-void cmd_mode(BaseSequentialStream *chp, int argc, char *argv[]) {
-    cmd_commands("mode", chp, argc, argv);
-}
-
-void cmd_commands(const char* name, BaseSequentialStream *chp, int argc, char *argv[]) {
-    AP_HAL::Shell::ShellCommand* scp = _shell_commands;
-    if (scp == NULL) {
-        chprintf(chp, "commands is empty\r\n");
-        return;
-    }
-
-    while (scp->sc_name != NULL) {
-        if (strcmp(scp->sc_name, name) == 0) {
-            scp->sc_function(argc, argv);
-            return;
-        }
-
-        scp++;
-    }
 }
