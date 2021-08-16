60021ed1b3392e4f81b5fef630d27ffcdbaf3973
yinlanshan <yinlanshan@chasing-innovation.com>
2021-08-16 19:52:42 +0800

    提交DS18B20温度传感器单线驱动

---
+++ libraries/CH_Drivers/DS18_Sensor.cpp
@@ -0,0 +1,302 @@
+#include "DS18_Sensor.h"
+
+#define DS18_DEGUG 0
+
+#if CONFIG_HAL_BOARD == HAL_BOARD_SITL
+#define PIN_MODE(pin, mode) do{}while(0)
+#define PIN_LOW(pin) do{}while(0)
+#define PIN_HIGH(pin) do{}while(0)
+#define READ_PIN(pin) 1
+#else
+#define PIN_MODE(pin, mode) palSetLineMode(pin, mode);
+#define PIN_LOW(pin) palWriteLine(pin, 0)
+#define PIN_HIGH(pin) palWriteLine(pin, 1)
+#define READ_PIN(pin) palReadLine(pin)
+#endif
+
+#define BIT_BAND_ADDR(ptr) ((uint32_t *)(0x22000000 + (((uint32_t) ptr) - 0x20000000) * 32))
+
+#define ROM_SKIP 0xCC
+#define ROM_READ 0x33
+#define CMD_CONVERT 0x44
+#define CMD_READ_PAD 0xBE
+#define CMD_WRITE_PAD 0x4E
+#define CMD_LOAD_PAD 0xB8
+#define CMD_STORE_PAD 0x48
+
+extern const AP_HAL::HAL &hal;
+
+static DS18_Sensor *_singleton = nullptr;
+
+static GPTConfig tim6_cfg = {
+        1000000, //1MHz
+        NULL,
+        0,
+        0
+};
+
+extern "C" {
+    static void tm_cb(GPTDriver *gptp)
+    {
+        _singleton->timer_cb(gptp);
+    }
+}
+
+DS18_Sensor::DS18_Sensor(uint32_t ch_cnt, const ioline_t *channels)
+{
+    _singleton = this;
+    if (ch_cnt > MAX_CHANNEL)
+        ch_cnt = MAX_CHANNEL;
+    channel_count = ch_cnt;
+
+    bit_index = 0;
+    conv_bit_band = BIT_BAND_ADDR(cmds);
+    read_bit_band = conv_bit_band + 2 * 8;
+
+    cmds[0] = ROM_SKIP;
+    cmds[1] = CMD_CONVERT;
+    cmds[2] = ROM_SKIP;
+    cmds[3] = CMD_READ_PAD;
+
+    for (int i = 0; i < ch_cnt; ++i)
+    {
+        pins[i] = channels[i];
+        PIN_MODE(pins[i], PAL_MODE_OUTPUT_PUSHPULL);
+        PIN_HIGH(pins[i]);
+        buf_bit_bands[i] = BIT_BAND_ADDR(channel_bufs[i]);
+    }
+    tim6_cfg.callback = tm_cb;
+    gptStart(&GPTD6, &tim6_cfg);
+
+    state = STATE_IDLE;
+}
+
+void DS18_Sensor::update()
+{
+    if (state == STATE_IDLE && channel_count > 0)
+    {
+        bit_index = 0;
+        state = STATE_RESET_1;
+        tick1 = 0;
+        for (int i = 0; i < channel_count; ++i)
+            temperature[i] = INVALID_TEMPERATURE;
+
+        gptStopTimer(&GPTD6);
+        gptStartOneShot(&GPTD6, 20); //20us
+    }
+}
+
+void DS18_Sensor::timer_cb(GPTDriver *gptp)
+{
+    switch (state)
+    {
+        case STATE_RESET_1:
+            if (tick1 == 0)
+            {
+                tick1 = 1;
+                all_pin_low();
+                gptStartOneShot(&GPTD6, 500); //low for 500us
+            } else if (tick1 == 1)
+            {
+                tick1 = 2;
+                all_pin_high();
+                gptStartOneShot(&GPTD6, 40); //delay 40us
+            } else
+            {
+                tick1 = 0;
+                if (sum_all_pin_in() == channel_count)
+                {
+                    //没有应答
+                    state = STATE_IDLE;
+#if DS18_DEGUG
+                    hal.shell->printf("No Ans\r\n");
+#endif
+                    return;
+                }
+                state = STATE_CONVERT;
+                bit_index = 0;
+                gptStartOneShot(&GPTD6, 460); //delay 40us
+            }
+            break;
+        case STATE_CONVERT:
+            if (write_bit(conv_bit_band[bit_index]))
+            {
+                if (++bit_index == 2 * 8)
+                {
+                    state = STATE_WAIT;
+                    tick1 = 0;
+                    bit_index = 0;
+                }
+            }
+            break;
+        case STATE_WAIT:
+            if (tick1 == 0)
+            {
+                tick1 = 1;
+                all_pin_high();
+                gptStartOneShot(&GPTD6, 10000); //high for 10ms
+            } else if (tick1 == 1)
+            {
+                tick1 = 2;
+                all_pin_low();
+                gptStartOneShot(&GPTD6, 5); //low for 5us
+            } else if (tick1 == 2)
+            {
+                tick1 = 3;
+                all_pin_high();
+                gptStartOneShot(&GPTD6, 5); //high for 5us
+            } else
+            {
+                tick1 = 0;
+                if (sum_all_pin_in() < channel_count)
+                {
+                    bit_index++;
+                    if (bit_index < 80) //800ms timeout
+                    {
+                        gptStartOneShot(&GPTD6, 5); //high for 5us
+                        break;
+                    }
+                }
+                else
+                {
+#if DS18_DEGUG
+                    hal.shell->printf("Wait %d\r\n", bit_index);
+#endif
+                }
+                bit_index = 0;
+                state = STATE_RESET_2;
+                gptStartOneShot(&GPTD6, 5); //high for 5us
+            }
+            break;
+        case STATE_RESET_2:
+            if (tick1 == 0)
+            {
+                tick1 = 1;
+                all_pin_low();
+                gptStartOneShot(&GPTD6, 500); //low for 500us
+            } else if (tick1 == 1)
+            {
+                tick1 = 0;
+                state = STATE_READ_CMD;
+                bit_index = 0;
+                all_pin_high();
+                gptStartOneShot(&GPTD6, 500); //high for 500us
+            }
+            break;
+        case STATE_READ_CMD:
+            if (write_bit(read_bit_band[bit_index]))
+            {
+                if (++bit_index == 2 * 8)
+                {
+                    state = STATE_READ_DATA;
+                    tick1 = 0;
+                    bit_index = 0;
+                }
+            }
+            break;
+        case STATE_READ_DATA:
+            if (tick1 == 0)
+            {
+                tick1 = 1;
+                all_pin_low();
+                gptStartOneShot(&GPTD6, 5); //low for 5us
+            } else if (tick1 == 1)
+            {
+                tick1 = 2;
+                all_pin_high();
+                gptStartOneShot(&GPTD6, 5); //high for 5us
+            } else if (tick1 == 2)
+            {
+                tick1 = 3;
+                all_pin_load(bit_index);
+                gptStartOneShot(&GPTD6, 55); //high for 55us
+            } else
+            {
+                tick1 = 0;
+                ++bit_index;
+                if (bit_index >= 8 * 5)
+                {
+                    state = STATE_IDLE;
+                    bit_index = 0;
+
+                    for (int i = 0; i < channel_count; ++i)
+                    {
+                        int16_t *p = (int16_t *)channel_bufs[i];
+                        temperature[i] = (*p)/16.0f;
+#if DS18_DEGUG
+                        for (int j = 0; j < 5; ++j)
+                        {
+                            hal.shell->printf("%X ", channel_bufs[i][j]);
+                        }
+                        hal.shell->printf("%.3f\r\n", temperature[i]);
+#endif
+                    }
+                } else
+                {
+                    //next bit
+                    gptStartOneShot(&GPTD6, 5); //high for 5us
+                }
+            }
+
+            break;
+    }
+}
+
+void DS18_Sensor::all_pin_low(void)
+{
+    for (int i = 0; i < channel_count; ++i)
+        PIN_LOW(pins[i]);
+}
+
+void DS18_Sensor::all_pin_high(void)
+{
+    for (int i = 0; i < channel_count; ++i)
+        PIN_HIGH(pins[i]);
+}
+
+void DS18_Sensor::all_pin_load(uint32_t bit_offset)
+{
+    for (int i = 0; i < channel_count; ++i)
+    {
+        buf_bit_bands[i][bit_offset] = READ_PIN(pins[i]);
+    }
+}
+
+uint32_t DS18_Sensor::sum_all_pin_in(void)
+{
+    uint32_t sum = 0;
+    for (int i = 0; i < channel_count; ++i)
+    {
+        sum += READ_PIN(pins[i]);
+    }
+    return sum;
+}
+
+bool DS18_Sensor::write_bit(uint32_t bit)
+{
+    if (tick1 == 0)
+    {
+        all_pin_low();
+        if (bit)
+        {
+            gptStartOneShot(&GPTD6, 5); //low for 5us
+            tick1 = 1;
+        } else
+        {
+            gptStartOneShot(&GPTD6, 60); //low for 60us
+            tick1 = 2;
+        }
+        return false;
+    }
+
+    if (tick1 == 1)
+    {
+        gptStartOneShot(&GPTD6, 60); //high for 60us
+    } else
+    {
+        gptStartOneShot(&GPTD6, 5); //high for 5us
+    }
+    tick1 = 0;
+    all_pin_high();
+    return true;
+}
---
+++ libraries/CH_Drivers/DS18_Sensor.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <stdint.h>
+#include <AP_HAL/AP_HAL.h>
+
+/*
+ * DS18B20 digital temperature sensor
+ * Driver is implemented with Timer interrupts
+ * Create by Yinlanshan 21-8-12
+ */
+
+#define INVALID_TEMPERATURE 1000.0f
+
+#define MAX_CHANNEL 2
+#define CHANNEL_BUF_SIZE 8
+
+#define STATE_IDLE          0
+#define STATE_RESET_1       1
+#define STATE_CONVERT       2
+#define STATE_WAIT          3
+#define STATE_RESET_2       4
+#define STATE_READ_CMD      5
+#define STATE_READ_DATA     6
+
+
+class DS18_Sensor
+{
+public:
+    DS18_Sensor(uint32_t channel_count, const ioline_t *channels);
+    void update();
+    void timer_cb(GPTDriver *gptp);
+    uint32_t sample_time(void)
+    {
+        return sample_ts;
+    }
+    float read(uint32_t channel)
+    {
+        if (channel >= MAX_CHANNEL)
+            return INVALID_TEMPERATURE;
+        return temperature[channel];
+    }
+private:
+    ioline_t pins[MAX_CHANNEL];
+    uint8_t channel_count = 0;
+    uint32_t sample_ts = 0;
+    float temperature[MAX_CHANNEL] = {INVALID_TEMPERATURE}; // degrees C
+    uint8_t state = STATE_IDLE;
+    uint8_t tick1;
+    uint8_t bit_index = 0;
+
+    uint8_t cmds[4];
+    uint8_t channel_bufs[MAX_CHANNEL][CHANNEL_BUF_SIZE];
+    uint32_t *conv_bit_band, *read_bit_band;
+    uint32_t *buf_bit_bands[MAX_CHANNEL];
+
+    void all_pin_low(void);
+    void all_pin_high(void);
+    void  all_pin_load(uint32_t bit_offset);
+    uint32_t sum_all_pin_in(void);
+    bool write_bit(uint32_t bit);
+};
