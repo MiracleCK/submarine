aa6ada704325317380fe8d6b814034d31636d15a
yinlanshan <yinlanshan@chasing-innovation.com>
2021-09-13 16:48:55 +0800

    改进Shell代码，让命令注册变为动态实现，避免添加新的命令行还要修改AP_HAL_ChibiOS/Shell.cpp。但是引入的最多支持16个命令的限制

--- libraries/AP_HAL_ChibiOS/Shell.cpp
+++ libraries/AP_HAL_ChibiOS/Shell.cpp
@@ -12,17 +12,59 @@
 using namespace ChibiOS;
 
 extern const AP_HAL::HAL & hal;
+static AP_HAL::Shell::ShellCommand* _shell_commands = NULL;
+
+#define MAX_SHELL_COMMAND_COUNT 16
+#define SHELL_CMD_STUFF(N) \
+static void SHELL_CMD_STUFF##N(BaseSequentialStream *chp, int argc, char *argv[]) \
+{ \
+    _shell_commands[N].sc_function(argc, argv); \
+}
+#define CHIBI_SHELL_COMMAND_SLOT(N) {NULL, SHELL_CMD_STUFF##N},
+
+#define REPEAT_16_MACRO(macro) \
+macro(0) \
+macro(1) \
+macro(2) \
+macro(3) \
+macro(4) \
+macro(5) \
+macro(6) \
+macro(7) \
+macro(8) \
+macro(9) \
+macro(10) \
+macro(11) \
+macro(12) \
+macro(13) \
+macro(14) \
+macro(15)
+
+REPEAT_16_MACRO(SHELL_CMD_STUFF)
+
+static ShellCommand chibi_shell_commands[MAX_SHELL_COMMAND_COUNT] = {
+        REPEAT_16_MACRO(CHIBI_SHELL_COMMAND_SLOT)
+};
 
 ShellConfig shell_cfg = {
   (BaseSequentialStream *)&HAL_STDOUT_SERIAL,
-  NULL
+  chibi_shell_commands
 };
 
 AP_HAL::HAL::Callbacks* g_factory_cb = NULL;
 
 void Shell::register_commands(AP_HAL::Shell::ShellCommand* commands)
 {
-    shell_cfg.hc_commands = (HalCommand *)commands;
+    _shell_commands = commands;
+    uint32_t n = 0;
+    while (n < MAX_SHELL_COMMAND_COUNT - 1)
+    {
+        if (commands[n].sc_name == NULL)
+            break;
+        chibi_shell_commands[n].sc_name = commands[n].sc_name;
+        n++;
+    }
+    chibi_shell_commands[n].sc_name = NULL;
 }
 
 void Shell::register_factory_cb(AP_HAL::HAL::Callbacks* cb)
