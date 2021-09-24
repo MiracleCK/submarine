615d8166266c1e6f797b1be3b08af5636bd28744
yinlanshan <yinlanshan@chasing-innovation.com>
2021-09-24 11:27:05 +0800

    初始化Neo灯带的DMA内存，否则没有初始化的内存数据会被灯带芯片忽略掉（例如点亮4号灯，但是因为1～3号灯内存未初始化，被1号灯珠忽略掉，最终只有1号灯被点亮）

--- ArduSub/system.cpp
+++ ArduSub/system.cpp
@@ -102,6 +102,10 @@ void Sub::init_ardupilot()
     init_rc_in();               // sets up rc channels from radio
     init_rc_out();              // sets up motors and output to escs
     hal.rcout->set_neopixel_num_LEDs(6, 4);
+    //neo leds must set initial value, otherwise led2～4 would not be lighted
+    hal.rcout->set_neopixel_rgb_data(6, 0xF, 0, 0, 0);
+    hal.rcout->neopixel_send();
+
     //  TODO: Restore below line. Removing it for Stabilize mode test only Yinlanshan 210706
     //  If below line is run, mode change back to manual again.
     //init_joystick();            // joystick initialization
